8fb4a0c4f95391b2af57045d134fb6f9d78e0253
shinmike <shinmike90@gmail.com>
2017-06-20 19:23:34 +0000

    removed 'blah' from readme

--- test_script.js
+++ test_script.js
@@ -10,15 +10,18 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+// connect to our database
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
+  // execute a query on our database
   client.query("SELECT $1::int AS number", ["1"], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
     console.log(result.rows[0].number); //output: 1
+    // disconnect the client
     client.end();
   });
 });
